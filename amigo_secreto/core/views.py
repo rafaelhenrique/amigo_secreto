0e0d2e0aabce7697651aa36bdbe15fc3e1ae2719
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2017-11-25 12:06:10 -0200

    Fixing csrf problem

--- amigo_secreto/core/views.py
+++ amigo_secreto/core/views.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import random
 
-from django.template.context import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.views.generic import View
 from django.utils import timezone
 
@@ -24,8 +23,7 @@ class Raffle(View):
         self.context['friend'] = None
         self.context['participant'] = None
         self.context['year'] = timezone.now().year
-        return render_to_response(self.template_name,
-                                  self.context, RequestContext(request))
+        return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
         raffle_form = RaffleForm(request.POST)
@@ -34,8 +32,7 @@ class Raffle(View):
         if raffle_form.is_valid():
             participant = raffle_form.cleaned_data.get('name')
             if participant.raffled:
-                render_to_response(self.template_name,
-                                   self.context, RequestContext(request))
+                return render(request, self.template_name, self.context)
             list_to_chose = Participant.objects.filter(
                 chosen=False).exclude(name=participant.name)
             friend = random.choice(list_to_chose)
@@ -47,5 +44,4 @@ class Raffle(View):
         self.context['form'] = raffle_form
         self.context['friend'] = friend
         self.context['participant'] = participant
-        return render_to_response(self.template_name,
-                                  self.context, RequestContext(request))
+        return render(request, self.template_name, self.context)
