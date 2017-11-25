c9f3efbaa2b5cc0d4da274a2f32629903a5f4036
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2017-11-25 11:16:42 -0200

    Fixing default of ALLOWED_HOSTS

--- amigo_secreto/settings.py
+++ amigo_secreto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default="", cast=Csv())
 
 # Application definition
 
